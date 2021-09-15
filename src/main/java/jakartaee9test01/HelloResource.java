7f01d5c4299e66c24477e466419bb06afcd5787b
Christoph A. Mikusch <camikusch@web.de>
2021-09-15 08:20:52 +0200

    wip

--- src/main/java/jakartaee9test01/HelloResource.java
+++ src/main/java/jakartaee9test01/HelloResource.java
@@ -2,12 +2,24 @@ package jakartaee9test01;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
 
 	@GET
+	@Produces(MediaType.TEXT_PLAIN)
 	public String sayHello() {
 		return "Hallo Welt!";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/{name}")
+	public String doGreeting(@PathParam("name") String name, @QueryParam("language") String language) {
+		return "Hallo, " + name + (language == null ? "with no language specified" : "with language: " + language);
 	}
 }
