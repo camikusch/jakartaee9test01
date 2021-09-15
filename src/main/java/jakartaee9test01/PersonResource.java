5ef621cfa45783241c742984ff43859e66fc1141
Christoph A. Mikusch <camikusch@web.de>
2021-09-15 08:21:12 +0200

    wip

---
+++ src/main/java/jakartaee9test01/PersonResource.java
@@ -0,0 +1,43 @@
+package jakartaee9test01;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/person")
+public class PersonResource {
+
+	@GET
+	@Path("/0")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getPerson0() {
+		return Response
+			      .status(Response.Status.OK)
+			      .entity(new Person("person0", 49))
+			      .build();
+	}
+
+	@GET
+	@Path("/1")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getPerson1() {
+		return Response
+			      .status(Response.Status.OK)
+			      .entity(new Person("person1", 49))
+			      .build();
+
+	}
+	
+	@GET
+	@Path("/2")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getPerson2() {
+		return Response
+			      .status(Response.Status.OK)
+			      .entity(new Person("person2", 49))
+			      .build();
+
+	}
+}
